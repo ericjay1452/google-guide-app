261e2f75168cbe2dae5858c294760da01bcbea2b
Ilesanmi-Erioluwa <codeineboy1452@gmail.com>
2022-07-04 00:03:32 +0100

    My first Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./index.css"
+
+import {Header, Places, Lists } from "./components/index"  
+
+function App() {
+  return (
+ <div>
+  <Header />
+  <Places />
+  <Lists />
+ </div>
+  );
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export  { default as Header} from "./Header/Header";
+export  { default as Places} from "./Places/Places";
+export  { default as Lists} from "./Lists/Lists";
